761b1b84028edcf25788687757dbeddd36461c56
Vadzim Sabadakha <vadriarh@gmail.com>
2025-02-13 15:58:05 +0100

    translate messages, homework solution lesson04part2task2

--- homeTask/ItStep/lesson04/homeTaskSetFilms.py
+++ homeTask/ItStep/lesson04/homeTaskSetFilms.py
@@ -1,9 +1,9 @@
 setFilms=set()
-for i in range(5):
+for indexOfFilm in range(5):
     while True:
-        setFilms.add(input(f"Vvedite nazvanie {i + 1} fil\'ma: "))
-        if len(setFilms) < i + 1:
-            print("Etot fil\'m uje nazvan")
+        setFilms.add(input(f"Введите название {indexOfFilm + 1} фильма: "))
+        if len(setFilms) < indexOfFilm + 1:
+            print("Этот фильм уже был назван.")
             continue
         break
 
