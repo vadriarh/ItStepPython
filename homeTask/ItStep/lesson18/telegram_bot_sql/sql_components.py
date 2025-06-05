6f2b5c4b675f155ef23499252b683044afecaece
Vadzim Sabadakha <vadriarh@gmail.com>
2025-06-06 00:21:22 +0200

    add hometask solution 18 Telegram sql (base logic)

--- homeTask/ItStep/lesson18/telegram_bot_sql/sql_components.py
+++ homeTask/ItStep/lesson18/telegram_bot_sql/sql_components.py
@@ -1,14 +1,14 @@
 import sqlite3
 
-DB_NAME = ("database.db")
+DB_NAME = "database.db"
 
 
-def connect():
+def connect_db():
     return sqlite3.connect(DB_NAME)
 
 
 def create_table_users():
-    with connect() as conn:
+    with connect_db() as conn:
         conn.execute("""
             CREATE TABLE IF NOT EXISTS users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -18,17 +18,17 @@ def create_table_users():
 
 
 def create_table_notes():
-    with connect() as conn:
+    with connect_db() as conn:
         conn.execute("""
             CREATE TABLE IF NOT EXISTS notes(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             telegram_id INTEGER NOT NULL,
-            content TEXT NOT NULL
+            content TEXT NOT NULL)
         """)
 
 
 def create_table_logs():
-    with connect() as conn:
+    with connect_db() as conn:
         conn.execute("""
             CREATE TABLE IF NOT EXISTS logs(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -38,40 +38,65 @@ def create_table_logs():
 
 
 def add_user(telegram_id: int, username: str):
-    with connect() as conn:
-        cursor = conn.cursor()
+    message = f"Пользователь {username} с ID {telegram_id} добавлен в базу."
+    logging_message(message)
+    with connect_db() as conn:
         try:
             conn.execute("""
                 INSERT INTO users (telegram_id, username)
                 VALUES (?, ?)
             """, (telegram_id, username))
-            conn.commit()
         except sqlite3.IntegrityError:
             print(f"Пользователь с ID {telegram_id} уже существует.")
         else:
-            print(f"Пользователь {username} с ID {telegram_id} добавлен в базу.")
+            return message
+
 
 
 def add_note(telegram_id: int, note: str):
-    with connect() as conn:
-        cursor = conn.cursor()
+    message = (f"Добавлена заметка \"{note}\""
+               f" для пользователя {telegram_id}")
+    logging_message(message)
+    with connect_db() as conn:
         conn.execute("""
-            INSERT INTO notes (telegram_id, note)
+            INSERT INTO notes (telegram_id, content)
             VALUES (?, ?)
         """, (telegram_id, note))
-        conn.commit()
 
-
-
-def get_user(telegram_id):
-    with connect() as conn:
-        cursor = conn.cursor("""
-            SELECT * FROM users WHERE telegram_id = ?
+def show_notes(telegram_id: int):
+    message = f"Пользователь {telegram_id} запросил список заметок."
+    logging_message(message)
+    with connect_db() as conn:
+        cursor = conn.execute("""
+            SELECT content FROM notes WHERE telegram_id = ?
             """, (telegram_id,))
+        return cursor.fetchall()
+
+
+
+def remove_notes(telegram_id: int):
+    message = f"Пользователь {telegram_id} удалил свои заметки."
+    logging_message(message)
+    with connect_db() as conn:
+        conn.execute("""
+        DELETE FROM notes
+        WHERE telegram_id = ?""", (telegram_id,))
+
+
+
+def logging_message(message: str):
+    with connect_db() as conn:
+        conn.execute("""
+               INSERT INTO logs (message)
+               VALUES (?)
+           """, (message,))
+        print(message)
+
+
+def get_username(telegram_id):
+    with connect_db() as conn:
+        cursor = conn.execute("""
+            SELECT * FROM users WHERE telegram_id = ?
+            """,(telegram_id,))
         user = cursor.fetchone()
-        return {
-            "id: ": user[0],
-            "username: ": user[1],
-            "telegram_id: ": user[2],
-            "created at: ": user[3]
-        }
+        return user[1]
--- homeTask/ItStep/lesson18/telegram_bot_sql/telegram_bot_engine.py
+++ homeTask/ItStep/lesson18/telegram_bot_sql/telegram_bot_engine.py
@@ -1,10 +1,80 @@
-from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, filters, CallbackContext, \
-    ApplicationBuilder, CallbackQueryHandler
+import filters
+from telegram import Update
+from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes, MessageHandler, filters
 from dotenv import load_dotenv
 
 import os
 
+from homeTask.ItStep.lesson18.telegram_bot_sql.sql_components import *
+
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN_ID")
-ADMIN_ID = os.getenv("ADMIN_ID")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    if user.id != ADMIN_ID:
+        add_user(user.id, user.username)
+        await update.message.reply_text(f"Привет, {user.username}! Ты зарегистрирован.")
+    else:
+        await update.message.reply_text(f"Привет, {user.username}. Вам присвоен статус Администратора.")
+
+
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    note = update.message.text[5::]
+    add_note(user_id, note)
+    await update.message.reply_text(f"Добавлена заметка: {note}")
+
+
+async def notes(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_id = update.effective_user.id
+    notes_content = show_notes(user_id)
+    if notes_content:
+        answer = "Список заметок: \n"
+        for note in notes_content:
+            answer += f"\t\t\t\t\t{note}\n"
+    else:
+        answer = "Нет заметок."
+    await update.message.reply_text(answer)
+
+
+async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    remove_notes(user.id)
+    await update.message.reply_text(f"Все заметки удалены.")
+
+async def button_handler(update: Update, context:ContextTypes.DEFAULT_TYPE):
+    query = update.callback_query
+    await query.answer()
+
+    if query.data == 'status':
+        await query.message.reply_text("Статус системы:\nCPU: 20%\nRAM: 30%")
+    elif query.data == 'about':
+        await query.message.reply_text("Этот бот создан для демонстрации возможностей Telegram API.")
+    elif query.data == 'telegram_url':
+        await query.message.reply_text(f"Вот мой Telegram: t.me/{query.from_user.username}")
+    elif query.data == 'git_url':
+        await query.message.reply_text("Вот мой GitHub: github.com/vadriarh")
+    else:
+        await query.message.reply_text("Неизвестная команда.")
+
+def main():
+    app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+    create_table_users()
+    create_table_notes()
+    create_table_logs()
+
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CommandHandler("add", add))
+    app.add_handler(CommandHandler("notes", notes))
+    app.add_handler(CommandHandler("clear", clear))
+    app.add_handlers(MessageHandler(filters.TEXT & ~filters.COMMAND, button_handler))
+    print("Bot is polling...")
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
