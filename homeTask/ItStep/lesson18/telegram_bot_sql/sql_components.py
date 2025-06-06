6b36609249e949225477d7941ca4ebd98d82c339
Vadzim Sabadakha <vadriarh@gmail.com>
2025-06-06 02:14:07 +0200

    add hometask solution 18 Telegram sql (add extended functional)

--- homeTask/ItStep/lesson18/telegram_bot_sql/sql_components.py
+++ homeTask/ItStep/lesson18/telegram_bot_sql/sql_components.py
@@ -1,6 +1,13 @@
 import sqlite3
+import os
 
-DB_NAME = "database.db"
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_NAME = os.getenv("DB_NAME")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+LIMIT_SLOTS = 20
 
 
 def connect_db():
@@ -13,7 +20,8 @@ def create_table_users():
             CREATE TABLE IF NOT EXISTS users(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             telegram_id INTEGER NOT NULL UNIQUE,
-            username TEXT NOT NULL)
+            username TEXT NOT NULL,
+            slots INTEGER)
         """)
 
 
@@ -38,50 +46,71 @@ def create_table_logs():
 
 
 def add_user(telegram_id: int, username: str):
-    message = f"Пользователь {username} с ID {telegram_id} добавлен в базу."
-    logging_message(message)
+    bot_message = ""
     with connect_db() as conn:
         try:
             conn.execute("""
-                INSERT INTO users (telegram_id, username)
-                VALUES (?, ?)
-            """, (telegram_id, username))
+                INSERT INTO users (telegram_id, username, slots)
+                VALUES (?, ?, ?)
+            """, (telegram_id, username, 0))
         except sqlite3.IntegrityError:
-            print(f"Пользователь с ID {telegram_id} уже существует.")
+            bot_message = f"Пользователь с ID {telegram_id} совершил попытку перерегистрации."
+            user_message = f"Вы уже были зарегистрированы под именем {username}."
         else:
-            return message
-
+            if telegram_id != ADMIN_ID:
+                user_message = f"Привет, {username}! Ты зарегистрирован."
+            else:
+                user_message = f"Привет, {username}. Вам присвоен статус Администратора."
+            bot_message = f"Пользователь {username} с ID {telegram_id} добавлен в базу."
+    logging_message(bot_message)
+    return user_message
 
 
 def add_note(telegram_id: int, note: str):
-    message = (f"Добавлена заметка \"{note}\""
-               f" для пользователя {telegram_id}")
+    user = get_user(telegram_id)
+    if user["slots"] < LIMIT_SLOTS:
+        with connect_db() as conn:
+            conn.execute("""
+               INSERT INTO notes (telegram_id, content)
+               VALUES (?, ?)
+           """, (telegram_id, note))
+        with connect_db() as conn:
+            conn.execute("""
+                UPDATE users
+                SET slots = ?
+                WHERE telegram_id = ?
+           """, (user["slots"]+1, telegram_id))
+        message = f"Добавлена заметка \"{note}\" для пользователя {telegram_id}"
+    else:
+        message = "Превышен лимит доступных слотов. Необходимо удалить заметки."
     logging_message(message)
-    with connect_db() as conn:
-        conn.execute("""
-            INSERT INTO notes (telegram_id, content)
-            VALUES (?, ?)
-        """, (telegram_id, note))
+
 
 def show_notes(telegram_id: int):
-    message = f"Пользователь {telegram_id} запросил список заметок."
-    logging_message(message)
     with connect_db() as conn:
         cursor = conn.execute("""
             SELECT content FROM notes WHERE telegram_id = ?
             """, (telegram_id,))
-        return cursor.fetchall()
-
+        notes = cursor.fetchall()
+    message = f"Пользователь {telegram_id} запросил список заметок."
+    logging_message(message)
+    return notes
 
 
 def remove_notes(telegram_id: int):
-    message = f"Пользователь {telegram_id} удалил свои заметки."
-    logging_message(message)
     with connect_db() as conn:
         conn.execute("""
-        DELETE FROM notes
-        WHERE telegram_id = ?""", (telegram_id,))
-
+            DELETE FROM notes
+            WHERE telegram_id = ?
+        """, (telegram_id,))
+    with connect_db() as conn:
+        conn.execute("""
+            UPDATE users
+            SET slots = 0
+            WHERE telegram_id = ?
+        """, (telegram_id,))
+    message = f"Пользователь {telegram_id} удалил свои заметки."
+    logging_message(message)
 
 
 def logging_message(message: str):
@@ -90,13 +119,40 @@ def logging_message(message: str):
                INSERT INTO logs (message)
                VALUES (?)
            """, (message,))
-        print(message)
+    print(message)
 
 
-def get_username(telegram_id):
+def get_user(telegram_id):
     with connect_db() as conn:
         cursor = conn.execute("""
             SELECT * FROM users WHERE telegram_id = ?
-            """,(telegram_id,))
+            """, (telegram_id,))
         user = cursor.fetchone()
-        return user[1]
+    return {
+        "id": user[0],
+        "telegram_id": user[1],
+        "username": user[2],
+        "slots": user[3]
+    }
+
+
+def search_note(note: str):
+    with connect_db() as conn:
+        cursor = conn.execute("""
+            SELECT telegram_id FROM notes WHERE content = ?
+            """, (note,))
+        users = cursor.fetchall()
+    return users
+
+
+def get_users():
+    with connect_db() as conn:
+        cursor = conn.execute("""
+            SELECT * FROM users""")
+        users = cursor.fetchall()
+    return users
+
+
+def change_limit_slots(new_limit: int):
+    global LIMIT_SLOTS
+    LIMIT_SLOTS = new_limit
