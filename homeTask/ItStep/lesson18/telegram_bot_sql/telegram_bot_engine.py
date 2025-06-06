54a99cfdac50c98e14703a98b25e1ec56b459e69
Vadzim Sabadakha <vadriarh@gmail.com>
2025-06-06 02:15:06 +0200

    add hometask solution 18 Telegram sql (code cleaning and simplification)

--- homeTask/ItStep/lesson18/telegram_bot_sql/telegram_bot_engine.py
+++ homeTask/ItStep/lesson18/telegram_bot_sql/telegram_bot_engine.py
@@ -1,9 +1,6 @@
-import filters
 from telegram import Update
-from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes, MessageHandler, filters
-from dotenv import load_dotenv
+from telegram.ext import CommandHandler, ApplicationBuilder, ContextTypes
 
-import os
 
 from homeTask.ItStep.lesson18.telegram_bot_sql.sql_components import *
 
@@ -14,11 +11,7 @@ ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user = update.effective_user
-    if user.id != ADMIN_ID:
-        add_user(user.id, user.username)
-        await update.message.reply_text(f"Привет, {user.username}! Ты зарегистрирован.")
-    else:
-        await update.message.reply_text(f"Привет, {user.username}. Вам присвоен статус Администратора.")
+    await update.message.reply_text(add_user(user.id, user.username))
 
 
 async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -45,21 +38,6 @@ async def clear(update: Update, context: ContextTypes.DEFAULT_TYPE):
     remove_notes(user.id)
     await update.message.reply_text(f"Все заметки удалены.")
 
-async def button_handler(update: Update, context:ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    await query.answer()
-
-    if query.data == 'status':
-        await query.message.reply_text("Статус системы:\nCPU: 20%\nRAM: 30%")
-    elif query.data == 'about':
-        await query.message.reply_text("Этот бот создан для демонстрации возможностей Telegram API.")
-    elif query.data == 'telegram_url':
-        await query.message.reply_text(f"Вот мой Telegram: t.me/{query.from_user.username}")
-    elif query.data == 'git_url':
-        await query.message.reply_text("Вот мой GitHub: github.com/vadriarh")
-    else:
-        await query.message.reply_text("Неизвестная команда.")
-
 def main():
     app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 
@@ -71,7 +49,6 @@ def main():
     app.add_handler(CommandHandler("add", add))
     app.add_handler(CommandHandler("notes", notes))
     app.add_handler(CommandHandler("clear", clear))
-    app.add_handlers(MessageHandler(filters.TEXT & ~filters.COMMAND, button_handler))
     print("Bot is polling...")
     app.run_polling()
 
